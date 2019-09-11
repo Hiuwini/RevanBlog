1acf9ff8339c68a5d10cf942cf322ba514dbbe48
ralvarado <hiuwini.17@gmail.com>
2019-09-11 16:16:12 -0600

    Detalles de entradas en el blog

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.listar_pub, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.utils import timezone
-
+from django.shortcuts import render, get_object_or_404
 from .models import Publicacion
 
 # Create your views here.
@@ -8,3 +8,7 @@ from .models import Publicacion
 def listar_pub(request):
     posts = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'blog/post_list.html', {'pubs': posts})
+def post_detail(request, pk):
+    #Publicacion.objects.get(pk=pk) obtiene el post con el id que venga de pk
+    post = get_object_or_404(Publicacion, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
